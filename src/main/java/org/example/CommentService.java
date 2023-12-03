cdcbf3d4c73c5f8ac9b8a371a31d765445a943b9
asrytikov <asrytikov@gmail.com>
2023-12-03 16:59:52 +0400

    Lesson 30

--- src/main/java/org/example/CommentService.java
+++ src/main/java/org/example/CommentService.java
@@ -1,2 +1,15 @@
-package org.example;public class CommentService {
+package org.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentService {
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    public CommentRepository getCommentRepository() {
+        return commentRepository;
+    }
 }
